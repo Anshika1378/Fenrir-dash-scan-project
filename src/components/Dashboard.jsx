ae2bc937a8a3e272f6902deb9700484db83eadcd
Anshika1378 <anshikaagarwal56890@gmail.com>
2026-03-03 16:52:44 +0530

    refactor: comment out unused import and add Link import

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,8 @@
-import { AiOutlineHome } from "react-icons/ai";
+// import { AiOutlineHome } from "react-icons/ai";
 import { useState } from "react";
+
 import { FiMenu } from "react-icons/fi"; 
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import radio from "../assets/media/radio.png";
 import apps from "../assets/media/apps.png";
 import briefing from "../assets/media/briefing.png";
